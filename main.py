dbf8c038966f6e74f86ebd5c72d16d0ffb8eb9ef
Okunola Orogun <146725180+aoorogun@users.noreply.github.com>
2024-03-28 13:39:21 +0000

    added some more interfaces

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ class App:
         self.register_new_user_button_main_window.place(x=750, y=400)
 
         self.webcam_label = util.get_img_label(self.main_window)
-        self.webcam_label.place(x=10, y=0, width=700, height=400)
+        self.webcam_label.place(x=10, y=0, width=700, height=500)
 
         self.add_webcam(self.webcam_label)
 
@@ -46,11 +46,48 @@ class App:
         pass
     
     def register_new_user(self):
-        pass
+        self.register_new_user_window = tk.Toplevel(self.main_window)
+        self.register_new_user_window.geometry("1200x520+370+120")
+
+        self.accept_button_register_new_user_window = util.get_button(self.register_new_user_window, 'Accept', 'green', self.accept_register_new_user_window, fg="black")
+        self.accept_button_register_new_user_window.place(x=750, y=300)
+
         
+        self.try_again_button_register_new_user_window = util.get_button(self.register_new_user_window, 'Try Again', 'black', self.try_again_register_new_user_window, fg="red")
+        self.try_again_button_register_new_user_window.place(x=750, y=400)
+
+        self.capture_label = util.get_img_label(self.register_new_user_window)
+        self.capture_label.place(x=10, y=0, width=700, height=500)
+        
+        self.add_img_to_label(self.capture_label)
+
+        self.entry_text_register_new_user = util.get_entry_text(self.register_new_user_window)
+        self.entry_text_register_new_user.place(x=750, y=150)
+
+        self.text_label_register_new_user = util.get_text_label(self.register_new_user_window,'Please input username:')
+        self.text_label_register_new_user.place(x=750, y=70)
+
+    def try_again_register_new_user_window(self):
+        self.register_new_user.destroy()
+
+    def add_img_to_label(self, label):
+        imgtk = ImageTk.PhotoImage(image = self.most_recent_capture_pil)
+        label.imgtk = imgtk
+        label.configure(image = imgtk)
+
+        self.register_new_user_capture = self.most_recent_capture_arr.copy()
+
+        self._label.after(20, self.process_webcam)
 
     def start(self):
         self.main_window.mainloop()
+
+    def accept_register_new_user_window(self):
+        pass
+
+    
+    def try_again_register_new_user_window(self):
+        pass
 
 if __name__ == "__main__":
     app = App()
